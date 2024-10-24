070c78656f32de6045a6e21d56038cdf00c4cbf1
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-10-24 18:14:08 -0400

    merge asteroids into repository

---
+++ asteroids/README.md
@@ -0,0 +1,12 @@
+# asteroids
+
+This game is a clone of the classic Asteroids game using Pygame and object-oriented programming concepts.
+
+Was built as part of the boot.dev **Build Asteroids** guided project.
+
+![game screenshot](screenshot_1.png)
+
+### Steps to setup the virtual env and installing pygame:
+1. `python3 -m venv venv`
+2. `source venv/bin/activate`
+3. `pip install -r requirements.txt`
---
+++ asteroids/circleshape.py
@@ -0,0 +1,28 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+
+    def detect_collision(self, other):
+        # calculate distance between centers of the two objects/circles
+        distance = self.position.distance_to(other.position)
+        # return true if distance is equal or less than the sum of both circles radii
+        return distance <= self.radius + other.radius
